fc3de5f8239eb99b56db9f9ee1a2e10c144a9386
SecondEdition <maxwellfth@gmail.com>
2020-06-22 11:17:48 -0600

    move about me modal to site navigation bar

--- client/src/App.js
+++ client/src/App.js
@@ -4,42 +4,18 @@ import Background from './components/background/';
 import Sidebar from './components/sidebar/';
 import Header from './components/header/';
 import Main from './components/main/';
-import Modal from './components/modal/';
-import AboutMe from './components/aboutme/';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      show: false
-    };
-  }
-  showModal = () => {
-    this.setState(prev=>({
-      show: !prev.show
-    }));
-  };
-  render() {  
-    return (
-      <Fragment>
-        <Background />
-        <div className="framework">
-          <Sidebar />
-          <Header />
-          <Main />
-          <button  onClick={e => {
-              this.showModal();
-          }}
-          > 
-          show Modal 
-          </button>
-        </div>
-        <Modal onClose={this.showModal} show={this.state.show}>
-          <AboutMe />
-        </Modal>
-      </Fragment>
-    );
-  }
+function App() {
+  return (
+    <Fragment>
+      <Background />
+      <div className="framework">
+        <Sidebar />
+        <Header />
+        <Main />
+      </div>
+    </Fragment>
+  );
 }
 
 export default App;
--- client/src/components/sitenav/index.js
+++ client/src/components/sitenav/index.js
@@ -1,45 +1,65 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import '../../App.css';
+import Modal from '../modal/';
+import AboutMe from '../aboutme/';
 
 
+class SiteNav extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            show: false
+        };
+        }
+        showModal = () => {
+        this.setState(prev=>({
+            show: !prev.show
+        }));
+    };
+    render() {
+        return (
+            <Fragment>
+                <nav role="navigation" className="site-nav">
+                    <ul className="site-nav-list">
+                        {/* link to portfolio page */}
+                        <li className="site-nav-list-item">
+                            <a 
+                                className="site-nav-link"
+                                href="#Works">
+                            Works
+                            </a>
+                        </li>
 
-function SiteNav() {
-  return (
-    <nav role="navigation" className="site-nav">
-        <ul className="site-nav-list">
-            {/* link to portfolio page */}
-            <li className="site-nav-list-item">
-                <a 
-                    className="site-nav-link"
-                    href="#Works">
-                Works
-                </a>
-            </li>
+                        {/* link to info overlay */}
+                        <li className="site-nav-list-item">
+                            <a 
+                                className="site-nav-link"
+                                href="#About"
+                                onClick={e => {
+                                this.showModal();
+                                }}
+                                >
+                            About
+                            </a>
+                        </li>
 
-            {/* link to info overlay */}
-            <li className="site-nav-list-item">
-                <a 
-                    className="site-nav-link"
-                    href="#About"
-                    onClick={e => {
-                    this.showModal();
-                    }}
-                    >
-                About
-                </a>
-            </li>
+                        {/* link to contact form overlay */}
+                        <li className="site-nav-list-item">
+                            <a 
+                                className="site-nav-link"    
+                                href="#Contact">
+                            Contact
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+                <Modal onClose={this.showModal} show={this.state.show}>
+                    <AboutMe />
+                </Modal>
+            </Fragment>
 
-            {/* link to contact form overlay */}
-            <li className="site-nav-list-item">
-                <a 
-                    className="site-nav-link"    
-                    href="#Contact">
-                Contact
-                </a>
-            </li>
-        </ul>
-    </nav>
-  );
+        );
+    }
 }
 
 export default SiteNav;
